0310613ff3384355a7a1097912fdc393fd3b1ae2
Palisha Bajracharya <palibaj@gmail.com>
2019-04-14 04:57:30 -0700

    2nd commit

--- src/bmicalculator/BMICalculator.java
+++ src/bmicalculator/BMICalculator.java
@@ -1,40 +1,47 @@
 
-package bmicalculator;
-
+import java.lang.Math;
 import java.util.Scanner;
-import static jdk.nashorn.tools.ShellFunctions.input;
-
 
 public class BMICalculator {
 
-   
     public static void main(String[] args) {
-        double weight;
-                int height;
-      Scanner SC= new Scanner(System.in);
       
-      System.out.println("Enter your weight in kg:");
-      weight= SC.nextDouble();
-      
-        System.out.println("Enter your height in meter:");
-        height= SC.nextInt();
-        
-        double bmi= weight/Math.pow(height, 2.0);
-        System.out.println("your BMI is:" +bmi);
-        
-        if (bmi<18.5){
-        System.out.println("Underweight");
+        Scanner input = new Scanner(System.in);
+double weight;
+        int feet;
+        int inches;
+
+        System.out.print("Enter weight in pounds: ");
+        weight = input.nextFloat();
+
+        System.out.print("Enter feet: ");
+        feet = input.nextInt();
+
+        System.out.print("Enter inches: ");
+        inches = input.nextInt();
+        double weightInKilos = weight * 0.453592;
+        double heightInMeters = (((feet * 12) + inches) * .0254);
+        double bmi = weightInKilos / Math.pow(heightInMeters, 2.0);
+
+        System.out.println("Your BMI is: " + bmi);
+
+        if (bmi < 18.5 ) {
+            System.out.print("Underweight");
+        }
+        else if (bmi >= 18.5 && bmi < 25) {
+            System.out.print("Normal");
+        }
+
+        else if (bmi >= 25 && bmi < 30) {
+            System.out.print("Overweight");
+        }
+
+        else if (bmi >= 30) {
+            System.out.print("Obese");
+        }
+
+
+
+        input.close();
     }
-        else if (bmi >= 18.5 && bmi<24.9){
-                System.out.println("Normal");
-                }
-        else if (bmi >= 24.9 && bmi<30){
-                System.out.println("Overweight");
-                }
-        else if (bmi >= 30){
-                System.out.println("Obesity");
-                }
-    } 
-      
-    }
-   
+}
